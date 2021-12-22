f447f50a4d1c0d94718e14b1b01d35bf2bfed7df
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-22 21:01:23 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x");
+                double x = double.Parse(Console.ReadLine());
+                double s = 0;
+                for (int i = 1, h = 1; i < n; i++, h += 2)
+                {
+                    long F = 1;
+                    for (int j = n - 1; j >= 1; j--)
+                    {
+                        F *= j;
+                    }
+
+                    s += -(Math.Pow(2 * x, x)) + (Math.Pow(2 * x, 4)) - (Math.Pow(2 * x, 6)) / F;
+                }
+                Console.WriteLine("S={0;f2)", s);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
